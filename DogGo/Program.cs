6998b4c641f22bbcd3aa73ebdac693dbb9f639c9
Mariana Mena <maranashokry@ahoo.com>
2023-04-03 13:36:57 -0500

    Add project files.

---
+++ DogGo/Program.cs
@@ -0,0 +1,38 @@
+using DogGo.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//Line Added ()Starup.cs
+builder.Services.AddTransient<IWalkerRepository, WalkerRepository>();
+
+
+//Add DI  Inject DB context inside the services of our application
+//builder.Services.AddDbContext<BloggieDbContext>(options =>
+//options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//var app = builder.Build();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
